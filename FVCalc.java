2833119f3d8563f37acadc5fc7bae64e668cd29b
Itamar Berkowitz <itamar.berkowitz@post.runi.ac.il>
2024-11-10 12:42:11 +0200

    Bill3 and FVCalc fix

--- FVCalc.java
+++ FVCalc.java
@@ -6,5 +6,6 @@ public class FVCalc {
 		int n = Integer.parseInt(args[2]); 
 		double futureValue = currentValue * Math.pow((1 + (rate / 100)), n); 
 		System.out.println("After " + n + " years, $" + currentValue + " saved at " + rate + "% will yield $" + (int)futureValue);
+		
 	}
 }
